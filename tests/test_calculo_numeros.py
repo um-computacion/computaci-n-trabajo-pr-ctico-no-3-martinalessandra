cb525a3fd2bdf4bfa7d2054b089ffe342094c81e
Martin <m.alessandra@alumno.um.edu.ar>
2025-04-15 22:05:51 -0300

    Cambios dentro del submódulo

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,8 +1,10 @@
 import unittest
+
 from src.exceptions import (
     ingrese_numero,
     NumeroDebeSerPositivo,
 )
+
 from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
@@ -12,7 +14,9 @@ class TestCalculoNumeros(unittest.TestCase):
         return_value='100'
     )
     def test_ingreso_feliz(self, patch_input):
+
         numero = ingrese_numero()
+
         self.assertEqual(numero, 100)
 
     @patch(  # este patch controla lo que hace el input
@@ -20,7 +24,9 @@ class TestCalculoNumeros(unittest.TestCase):
         return_value='-100'
     )
     def test_ingreso_negativo(self, patch_input):
+
         with self.assertRaises(NumeroDebeSerPositivo):
+
             ingrese_numero()
 
     @patch(  # este patch controla lo que hace el input
@@ -28,7 +34,9 @@ class TestCalculoNumeros(unittest.TestCase):
         return_value='AAA'
     )
     def test_ingreso_letras(self, patch_input):
+
         with self.assertRaises(ValueError):
+            
             ingrese_numero()
 
 if __name__ == '__main__':
